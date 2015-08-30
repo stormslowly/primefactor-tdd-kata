b26dff302a6926cea620cc073eab7a7719229c97
stormslowly@gmail.com <stormslowly@gmail.com>
2015-08-30 09:49:49 +0800

    add the empty function

--- test/primeFactorTest.js
+++ test/primeFactorTest.js
@@ -1,6 +1,8 @@
 'use strict';
 var expect = require('chai').expect;
 
+var PrimeFactor = require('../lib/primefactor.js');
+
 describe('PrimeFactor', function () {
 
     it('primeFactor of 1 is empty', function () {
