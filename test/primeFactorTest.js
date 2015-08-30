f186aaf3c2b3de041b77651e5db5b3808e62e905
stormslowly@gmail.com <stormslowly@gmail.com>
2015-08-30 10:40:46 +0800

    case failed again

--- test/primeFactorTest.js
+++ test/primeFactorTest.js
@@ -9,7 +9,11 @@ describe('PrimeFactor', function () {
         expect(PrimeFactor.of(1)).to.deep.equal([]);
         expect(PrimeFactor.of(2)).to.deep.equal([2]);
         expect(PrimeFactor.of(3)).to.deep.equal([3]);
-        expect(PrimeFactor.of(4)).to.deep.equal([2,2]);
+        expect(PrimeFactor.of(4)).to.deep.equal([2, 2]);
+        expect(PrimeFactor.of(5)).to.deep.equal([5]);
+        expect(PrimeFactor.of(6)).to.deep.equal([2, 3]);
+        expect(PrimeFactor.of(7)).to.deep.equal([7]);
+        expect(PrimeFactor.of(8)).to.deep.equal([2,2,2]);
 
     });
 
