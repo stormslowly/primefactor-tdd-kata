ffda68a0317df0fe92a96b5bc95bbcf44a5420ed
stormslowly@gmail.com <stormslowly@gmail.com>
2015-08-30 09:47:49 +0800

    write the expect first,ignore the error first

--- test/primeFactorTest.js
+++ test/primeFactorTest.js
@@ -3,8 +3,8 @@ var expect = require('chai').expect;
 
 describe('PrimeFactor', function () {
 
-    it('should fail', function () {
-        expect(1).to.equal(1);
+    it('primeFactor of 1 is empty', function () {
+        expect(PrimeFactor.of(1)).to.deep.equal([]);
     });
 
 });
