d15bbdac40314c48e0eedb21d2b5cd64482bc885
stormslowly@gmail.com <stormslowly@gmail.com>
2015-08-30 10:37:06 +0800

    fix the 4 => [2,2]

--- test/primeFactorTest.js
+++ test/primeFactorTest.js
@@ -8,6 +8,8 @@ describe('PrimeFactor', function () {
     it('primeFactor dive number to prime array', function () {
         expect(PrimeFactor.of(1)).to.deep.equal([]);
         expect(PrimeFactor.of(2)).to.deep.equal([2]);
+        expect(PrimeFactor.of(3)).to.deep.equal([3]);
+        expect(PrimeFactor.of(4)).to.deep.equal([2,2]);
 
     });
 
