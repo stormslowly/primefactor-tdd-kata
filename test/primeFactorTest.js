15f283339c000255b63425bb83f197398832faaf
stormslowly@gmail.com <stormslowly@gmail.com>
2015-08-30 09:42:07 +0800

    test framework works

---
+++ test/primeFactorTest.js
@@ -0,0 +1,11 @@
+'use strict';
+/* global it,describe*/
+var expect = require('chai').expect;
+
+describe('PrimeFactor', function () {
+
+    it('should fail', function () {
+        expect(1).to.equal(2);
+    });
+
+});
